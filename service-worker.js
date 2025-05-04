c570c9ad105777f9b5683ef412f231756d3ccd2d
unknown <hcamusso@gmail.com>
2025-05-04 09:47:07 -0300

    Camer Codex v 1.0 serviceworker

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,49 @@
-self.addEventListener('install', (e) => {
-    console.log('Service Worker instalado');
-    self.skipWaiting();
-  });
-  
-  self.addEventListener('fetch', (e) => {
-    e.respondWith(fetch(e.request));
-  });
-  
+const CACHE_NAME = 'camer-codex-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/manifest.json',
+  '/favicon.ico',
+  '/icon-192.png',
+  '/icon-512.png',
+  // Agregá aquí cualquier carpeta o archivo extra que uses:
+  '/audios/cartas.json',
+  '/css/',
+  '/audios/'
+];
+
+self.addEventListener('install', (event) => {
+  console.log('[ServiceWorker] Installing...');
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('[ServiceWorker] Caching app shell');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  console.log('[ServiceWorker] Activating...');
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== CACHE_NAME) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
+});
