1bd58506ecb94a3a66112ae5db82e65a952db1f7
unknown <hcamusso@gmail.com>
2025-06-27 17:17:29 -0300

    Battery1.01

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
 // config.js
-const appVersion = "1.0 Vainilla";  // Aquí puedes cambiar la versión cuando sea necesario.
+const appVersion = "1.01 Vainilla";  // Aquí puedes cambiar la versión cuando sea necesario.
