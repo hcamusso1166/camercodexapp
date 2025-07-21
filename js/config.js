85e188f9899a0430b9c94693e4e1a79be2a6b3fa
unknown <hcamusso@gmail.com>
2025-07-21 12:31:13 -0300

    vainlla 1.02

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
 // config.js
-const appVersion = "1.01 Vainilla";  // Aquí puedes cambiar la versión cuando sea necesario.
+const appVersion = "1.02 Vainilla";  // Aquí puedes cambiar la versión cuando sea necesario.
