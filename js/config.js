1c35cd24d2b3a7e8b05595fe9fabd777074e679f
unknown <hcamusso@gmail.com>
2025-08-29 08:15:56 -0300

    1.0 PEMAR

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
 // config.js
-const appVersion = "1.04 cache";  // Aquí puedes cambiar la versión cuando sea necesario.
+const appVersion = "1.00 PEMAR";  // Aquí puedes cambiar la versión cuando sea necesario.
