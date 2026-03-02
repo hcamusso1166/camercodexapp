aa8af8bf1a0141a70b3a8befedb1060f9717bd68
unknown <hcamusso@gmail.com>
2026-03-02 20:21:09 -0300

    actualizaciones

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
 // config.js
-const appVersion = "1.00 Membresía";  // Aquí puedes cambiar la versión cuando sea necesario.
+const appVersion = "1.01 Membresía 1";  // Aquí puedes cambiar la versión cuando sea necesario.
