59f7eaf70d54a4ab08b988e9a6cdb3cc78d279b9
unknown <hcamusso@gmail.com>
2026-03-16 08:03:18 -0300

    latido

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
 // config.js
-const appVersion = "1.08 Pre";  // Aquí puedes cambiar la versión cuando sea necesario.
+const appVersion = "1.08 Latido";  // Aquí puedes cambiar la versión cuando sea necesario.
