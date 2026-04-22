0e989f87f9e9448d16bcc7810e577ed345a59033
unknown <hcamusso@gmail.com>
2026-04-22 19:28:36 -0300

    1.11 ayuda

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
 // config.js
-const appVersion = "1.10 Ayuda";  // Aquí puedes cambiar la versión cuando sea necesario.
+const appVersion = "1.11 Ayuda";  // Aquí puedes cambiar la versión cuando sea necesario.
